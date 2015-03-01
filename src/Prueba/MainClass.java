2bb9739d45f1bd821d13177c756ebb702431f671
Raul Burgos Murray <raulburgos007@gmail.com>
2015-03-01 02:11:33 -0400

    Subida de eclipse

---
+++ src/Prueba/MainClass.java
@@ -0,0 +1,22 @@
+package Prueba;
+
+import java.util.Scanner;
+
+public class MainClass {
+
+	public static void main(String[] args){
+		System.out.println("Por favor escriba algo");
+		String textoTeclado;
+		Scanner textoScanner;
+		int longitudTexto=0;
+		int caracterActual=0;
+		textoScanner=new Scanner(System.in);
+		textoTeclado=textoScanner.nextLine();
+		longitudTexto=textoTeclado.length();
+		System.out.println("El texto ingresado tiene " + longitudTexto + " caracteres");
+		while(caracterActual<longitudTexto){
+			System.out.println(textoTeclado.substring(caracterActual, caracterActual+1).toUpperCase());
+			caracterActual++;
+		}
+	}
+}
